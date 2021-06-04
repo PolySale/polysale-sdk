fcb37e3de2ab31a08de48fd19ffed0a2f5807bc9
user <email@example.com>
2021-06-04 17:16:32 +0200

    WMATIC

--- src/entities/token.ts
+++ src/entities/token.ts
@@ -59,10 +59,10 @@ export function currencyEquals(currencyA: Currency, currencyB: Currency): boolea
 export const WETH = {
   [ChainId.MAINNET]: new Token(
     ChainId.MAINNET,
-    '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+    '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270',
     18,
-    'WBNB',
-    'Wrapped BNB'
+    'WMATIC',
+    'Wrapped Matic'
   ),
   [ChainId.TESTNET]: new Token(ChainId.TESTNET, '0xaE8E19eFB41e7b96815649A6a60785e1fbA84C1e', 18, 'WBNB', 'Wrapped BNB')
 }
